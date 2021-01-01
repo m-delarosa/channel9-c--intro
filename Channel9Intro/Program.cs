1a39fc1377dfc2cf496459056e7c27bf418944c8
github username <56710845+m-delarosa@users.noreply.github.com>
2021-01-01 10:10:08 -0700

    Add final fibonacci solution.

--- Channel9Intro/Program.cs
+++ Channel9Intro/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Channel9Intro
 {
     class Program
     {
+        static void ExploreIf()
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            int c = 4;
+            if ((a + b + c > 10) && (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not greater than the second");
+            }
+
+            if ((a + b + c > 10) || (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not greater than the second");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("CONSOLE RESET");
+
+            var fibonacciNumbers = new List<int> {1, 1};
+
+            while (fibonacciNumbers.Count <= 19)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                int sum = previous + previous2;
+                fibonacciNumbers.Add(sum);
+            }
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
     }
 }
